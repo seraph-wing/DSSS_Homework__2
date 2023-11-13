e3d8e325fcf9ae3803cf9061d367c27bcc604ab6
vishw <vishwang.dave@fau.de>
2023-11-13 10:12:24 +0100

    debugged setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    # Other details like author, description, requirements, etc.
+    author='Vishwang',
+    description='A simple math quiz',
+    license='MIT',
+    keywords='math quiz'
+)
